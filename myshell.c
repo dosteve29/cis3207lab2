b46cd1df1d699ff9810fceebdcb288fd27490f67
Steve Do <gustmd1210@gmail.com>
2017-09-27 02:01:23 -0400

    started functions

--- myshell.c
+++ myshell.c
@@ -1,7 +1,30 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include <stdio.h>
 
+void printPrompt();
+char * readLine();
+
 int main(int argc, char ** argv){
-    printf("Hello World!\n");
+    char * line;
+
+    while(1){
+        printPrompt();
+        line = readLine();
+        printf("%s", line);
+    }
 
     return 0;
+}
+
+void printPrompt(){
+    printf("%s: ", "Prompt"); 
+}
+
+char * readLine(){
+    char * line = NULL;
+    size_t buf = 0;
+    getline(&line, &buf, stdin);
+    return line;
 }
